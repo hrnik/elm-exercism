0990e0a7de13f1d49667c215be530b876266891c
Nikita Khristinin <nkhristinin@gmail.com>
2016-12-29 23:28:32 +0200

    bob, bob, bob

--- bob/Bob.elm
+++ bob/Bob.elm
@@ -1,1 +1,59 @@
 module Bob exposing (..)
+
+import Char
+import Regex
+
+
+hey : String -> String
+hey phrase =
+    if phrase |> String.trim |> String.isEmpty then
+        "Fine. Be that way!"
+    else if isShout phrase then
+        "Whoa, chill out!"
+    else if isQuestion phrase then
+        "Sure."
+    else
+        "Whatever."
+
+
+isShout : String -> Bool
+isShout phrase =
+    removeChars phrase
+        |> String.words
+        |> removeAcronyms
+        |> List.any isUpperString
+
+
+isUpperString : String -> Bool
+isUpperString phrase =
+    List.all Char.isUpper (String.toList phrase)
+
+
+isQuestion : String -> Bool
+isQuestion phrase =
+    case lastChar phrase of
+        Nothing ->
+            False
+
+        Just a ->
+            "?" == String.fromChar a
+
+
+lastChar : String -> Maybe Char
+lastChar phrase =
+    List.head (List.reverse (String.toList phrase))
+
+
+removeChars : String -> String
+removeChars phrase =
+    String.filter removeChar phrase
+
+
+removeChar : Char -> Bool
+removeChar char =
+    not (List.member char [ '!', '?' ])
+
+
+removeAcronyms : List String -> List String
+removeAcronyms phrases =
+    List.filter (\x -> x /= "OK") phrases
