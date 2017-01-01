53ae8ac3f4757006bbf7940d9b0f1f6aea0e2dd6
Nikita Khristinin <nkhristinin@gmail.com>
2017-01-01 17:34:52 +0200

    done

--- leap/Leap.elm
+++ leap/Leap.elm
@@ -1,1 +1,12 @@
 module Leap exposing (..)
+
+
+isLeapYear : Int -> Bool
+isLeapYear year =
+    if rem year 4 == 0 then
+        if rem year 100 == 0 && rem year 400 /= 0 then
+            False
+        else
+            True
+    else
+        False
