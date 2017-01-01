29d629a0f831d5793d155b0c8fb784121e4a65f3
Nikita Khristinin <nkhristinin@gmail.com>
2017-01-01 21:53:19 +0200

    done :sunglasses:

--- pangram/Pangram.elm
+++ pangram/Pangram.elm
@@ -1,6 +1,17 @@
 module Pangram exposing (..)
 
+import Char
+import Set
+
 
 isPangram : String -> Bool
 isPangram phrase =
-    True
+    Set.size (toLettersOnly phrase) == 26
+
+
+toLettersOnly : String -> Set.Set Char
+toLettersOnly phrase =
+    String.toLower phrase
+        |> String.filter (\x -> Char.isLower x)
+        |> String.toList
+        |> Set.fromList
