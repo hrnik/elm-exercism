477f18afe40aa664cb3e9789506f489d23f945a3
Nikita Khristinin <nkhristinin@gmail.com>
2017-01-07 18:10:54 +0200

    done wordCount

--- word-count/WordCount.elm
+++ word-count/WordCount.elm
@@ -1,4 +1,32 @@
 module WordCount exposing (..)
 
+import Dict exposing (Dict)
+import Char
+
 
 wordCount : String -> Dict String Int
+wordCount phrase =
+    prepareString phrase
+        |> String.words
+        |> List.map (\x -> ( x, (wordContain (prepareString phrase) x) ))
+        |> Dict.fromList
+
+
+wordContain : String -> String -> Int
+wordContain phrase word =
+    String.words phrase
+        |> List.filter (\x -> x == word)
+        |> List.length
+
+
+prepareString : String -> String
+prepareString phrase =
+    String.toLower phrase
+        |> String.words
+        |> List.map (\x -> wordWithoutPunctuation x)
+        |> String.join " "
+
+
+wordWithoutPunctuation : String -> String
+wordWithoutPunctuation word =
+    String.filter (\x -> Char.isLower x || Char.isDigit x) word
