d74774dd65f9ae22f2c007ca3cbd88cc487b324b
Nikita Khristinin <nkhristinin@gmail.com>
2017-01-08 13:43:44 +0200

    done sum-of-multiplies

--- sum-of-multiples/SumOfMultiples.elm
+++ sum-of-multiples/SumOfMultiples.elm
@@ -1,1 +1,24 @@
 module SumOfMultiples exposing (..)
+
+import Set
+
+
+sumOfMultiples : List Int -> Int -> Int
+sumOfMultiples multiples number =
+    List.map (\x -> listMultiple x number) multiples
+        |> List.concat
+        |> unicArray
+        |> List.sum
+
+
+listMultiple : Int -> Int -> List Int
+listMultiple multiple number =
+    List.range 1 (number - 1)
+        |> List.filter (\x -> x % multiple == 0)
+
+
+unicArray : List comparable -> List comparable
+unicArray arr =
+    arr
+        |> Set.fromList
+        |> Set.toList
