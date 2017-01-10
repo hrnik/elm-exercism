291769f99344c855f5af89a3f51fbc6e101eaa76
Nikita Khristinin <nkhristinin@gmail.com>
2017-01-10 21:23:12 +0200

    done, map2 is supercool

--- hamming/Hamming.elm
+++ hamming/Hamming.elm
@@ -6,17 +6,7 @@ distance firstDNA secondDNA =
     if String.length firstDNA /= String.length secondDNA then
         Nothing
     else
-        Just (difference (String.toList firstDNA) (String.toList secondDNA) 0)
-
-
-difference : List Char -> List Char -> Int -> Int
-difference firstArr secondArr acc =
-    case firstArr of
-        [] ->
-            acc
-
-        first :: rest ->
-            if Just first /= List.head secondArr then
-                difference rest (List.drop 1 secondArr) (acc + 1)
-            else
-                difference rest (List.drop 1 secondArr) acc
+        List.map2 (/=) (String.toList firstDNA) (String.toList secondDNA)
+            |> List.filter identity
+            |> List.length
+            |> Just
