d8b5bc20fee52b4b2058336d33e5fd2141b30a2b
Nikita Khristinin <nkhristinin@gmail.com>
2017-01-10 21:14:16 +0200

    done, not super. but working

--- hamming/Hamming.elm
+++ hamming/Hamming.elm
@@ -3,4 +3,20 @@ module Hamming exposing (..)
 
 distance : String -> String -> Maybe Int
 distance firstDNA secondDNA =
-    Just 0
+    if String.length firstDNA /= String.length secondDNA then
+        Nothing
+    else
+        Just (difference (String.toList firstDNA) (String.toList secondDNA) 0)
+
+
+difference : List Char -> List Char -> Int -> Int
+difference firstArr secondArr acc =
+    case firstArr of
+        [] ->
+            acc
+
+        first :: rest ->
+            if Just first /= List.head secondArr then
+                difference rest (List.drop 1 secondArr) (acc + 1)
+            else
+                difference rest (List.drop 1 secondArr) acc
